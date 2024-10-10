eb0fe1c7ff4c1d37a48fa25a7b5a428881753835
sultanprabowo90@gmail.com <sultanprabowo90@gmail.com>
2024-10-10 21:26:03 +0700

    modul2_codelab_mobileC

---
+++ lib/app/modules/components/appbar_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../routes/app_pages.dart';
+
+// ignore: must_be_immutable
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  String titleText;
+  CustomAppBar({
+    super.key,
+    required this.titleText,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(titleText),
+      actions: <Widget>[
+        PopupMenuButton<int>(
+          onSelected: (value) {
+            switch (value) {
+              case 0:
+                Get.toNamed(Routes.HTTP);
+                break;
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 0,
+              child: TextButton(
+                onPressed: null,
+                child: Text("HTTP Page"),
+              ),
+            ),
+            const PopupMenuItem(
+              value: 1,
+              child: TextButton(
+                onPressed: null,
+                child: Text("Dio Page"),
+              ),
+            ),
+            const PopupMenuItem(
+              value: 2,
+              child: TextButton(
+                onPressed: null,
+                child: Text("GetConnect Page"),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
